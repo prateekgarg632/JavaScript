4cb31f1ea43b3b4575b9fdbe8589954f200a63af
prateekgarg632 <36416683+prateekgarg632@users.noreply.github.com>
2023-08-25 18:23:57 +0000

    conversion of datatypes

---
+++ 01-basics/conversionOperations.js
@@ -0,0 +1,12 @@
+let score="123abc"
+
+// console.log(typeof score);
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+let value="prateek"
+
+let booleanValue=Boolean(value)
+console.log(booleanValue);
