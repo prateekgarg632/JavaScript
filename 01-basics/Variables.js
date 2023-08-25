6d9b7b6cb6cc40906945613347a0f4b1bd729c8d
prateekgarg632 <36416683+prateekgarg632@users.noreply.github.com>
2023-08-25 18:03:22 +0000

    Variables in JavaScript

---
+++ 01-basics/Variables.js
@@ -0,0 +1,15 @@
+const accountNumber=12345
+let accountName="Prateek"
+var accountEmail="prateek@gmial.com"
+accountCity="Delhi"
+let accountState;
+console.log(accountNumber);
+
+// accountNumber=5698 not allowed for const variable
+
+console.table([accountNumber,accountEmail,accountName,accountCity,accountState])
+
+/*
+prefer not to use var 
+because of issue in block scope & functional scope
+*/
