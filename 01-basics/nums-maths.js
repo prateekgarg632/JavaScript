43f786748ca1e302ed37c71a6bb95268cba617b9
prateekgarg632 <36416683+prateekgarg632@users.noreply.github.com>
2023-08-26 18:02:39 +0000

    Numbers&Maths

---
+++ 01-basics/nums-maths.js
@@ -0,0 +1,21 @@
+const score=400;
+// console.log(score);
+
+// const balance =new Number(199);
+// console.log(balance);
+// console.log(balance.toFixed(2))
+
+const hundreds=1000000;
+// console.log(hundreds.toLocaleString());
+
+
+//**************Maths********************
+
+// console.log(Math)
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.55));
+
+// console.log(Math.floor(Math.random()*10+1));
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*10+1+min));
