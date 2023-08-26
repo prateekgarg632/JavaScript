22d763950203f26b9f384dbb4e5e057a3637be05
prateekgarg632 <36416683+prateekgarg632@users.noreply.github.com>
2023-08-26 13:03:53 +0000

    Strings

---
+++ 01-basics/Strings.js
@@ -0,0 +1,13 @@
+let name="Prateek"
+let lastname="Garg"
+let fullname=name+lastname;
+//console.log(fullname);
+// console.log(`My name is ${name} and my last name is ${lastname}`);
+
+const username=new String("https://www.publiccity.com.hypercaresupport.officials");
+//console.log(username.indexOf('a'));
+const user=username.substring(2,8);
+// console.log(user);
+//console.log(username.trimEnd());
+//console.log(username.replace(' ','k'));
+const breaks=username.split('.');
