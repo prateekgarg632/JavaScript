f59d6e9d030832c57403c269ed9b7e5f7f7e0df2
prateekgarg632 <36416683+prateekgarg632@users.noreply.github.com>
2023-08-26 18:32:31 +0000

    Dates

---
+++ 01-basics/Dates.js
@@ -0,0 +1,17 @@
+let myDate=new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toISOString());
+
+// let newdate=new Date(2023,1,25);
+// console.log(newdate.toString());
+// console.log(newdate.toLocaleString());
+
+let createdDate=new Date(2023,1,20,6,9);
+// console.log(createdDate.toLocaleString());
+
+let newdate=new Date("01-24-2025")
+console.log(newdate.toLocaleString());
