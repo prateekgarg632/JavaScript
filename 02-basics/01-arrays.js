f311c5c2d83857ad434d109101792b11a69a7acd
prateekgarg632 <36416683+prateekgarg632@users.noreply.github.com>
2023-08-27 08:51:09 +0000

    Arrays-basics

---
+++ 02-basics/01-arrays.js
@@ -0,0 +1,10 @@
+const myarr=[1,2,3,4];
+// console.log(myarr);
+// console.log(myarr.length);
+const arr1=myarr.slice(1,3);
+// console.log(arr1);
+ console.log(myarr);
+
+const arr2=myarr.splice(1,3);
+console.log(arr2);
+console.log(myarr);
